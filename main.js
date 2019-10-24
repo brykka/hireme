d3b3a8c57f3041d25c4312fedbd0464eda4c0bc2
theyij <theyijen@gmail.com>
2019-10-24 17:09:58 +0900

    add contact icon for each

--- main.js
+++ main.js
@@ -59,14 +59,42 @@ app.get('/indextest', function (req, res) { // req = incoming request, res = out
   var person1Image = 'https://github.com/theyij.png?size=200.src'
   var person2Image = 'https://github.com/Miimii1010.png?size=200'
   var person3Image = 'https://avatars1.githubusercontent.com/u/47349194?s=460&v=4'
-
+  var linkedin = 'https://image.flaticon.com/icons/svg/1384/1384014.svg'
+  var github = 'https://image.flaticon.com/icons/svg/25/25231.svg'
+  var website = 'https://image.flaticon.com/icons/svg/1242/1242392.svg'
 
   res.render('index', { // res = outgoing response
+    icon: {
+    	linkedin: linkedin,
+    	github: github,
+    	site: website
+    },
     listOfQuizzes: quizzes,
     body: story,
-    person1: {name: person1, bio: "Currently looking for new challenges and opportunities to contribute in Front-End web-development. ", contact: "linkdin", avatar: person1Image },
-    person2: {name: person2, bio: "This is a little bit of info about me. Here is my history, current and future projects. This is what I am intereseted in.", contact: "gmail", avatar: person2Image },
-    person3: {name: person3, bio: "This is a little bit of info about me. Here is my history, current and future projects. This is what I am intereseted in.", contact: "twitter", avatar: person3Image }
+    person1: {
+    	name: person1, 
+    	bio: "Currently looking for new challenges and opportunities to contribute in Front-End web-development. ", 
+    	avatar: person1Image,
+    	linkedin: "https://www.linkedin.com/in/yangijen/",
+    	github:"https://github.com/theyij",
+    	website:"https://yijen.netlify.com"
+    },
+    person2: {
+    	name: person2, 
+    	bio: "This is a little bit of info about me. Here is my history, current and future projects. This is what I am intereseted in.", 
+    	avatar: person2Image,
+    	linkedin: "https://www.linkedin.com/",
+    	github:"https://github.com/Miimii1010",
+    	website:"https://codepen.io/"
+    },
+    person3: {
+    	name: person3, 
+    	bio: "This is a little bit of info about me. Here is my history, current and future projects. This is what I am intereseted in.", 
+    	avatar: person3Image,
+    	linkedin: "https://www.linkedin.com/",
+    	github:"https://github.com/brykka",
+    	website:"https://codepen.io/"
+    }
   })
 })
 
