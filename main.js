a98b5b0e59c78610d5bfa0be1ce67414706bf02f
theyij <theyijen@gmail.com>
2019-11-08 17:36:45 +0900

    connect all db to it's quiz page

--- main.js
+++ main.js
@@ -24,46 +24,33 @@ function closeDB() {
 // js file is included here:
 var fs = require('fs');
 eval(fs.readFileSync('./pages/index.js') + '');
-eval(fs.readFileSync('./pages/html.js') + '');
+// eval(fs.readFileSync('./pages/html.js') + '');
 // eval(fs.readFileSync('./pages/css.js') + '');
 eval(fs.readFileSync('./pages/javascript.js') + '');
 
 
+// HTML stuff here
+app.get('/quiz/html', function(req, res) {
+    var html = require('./pages/html.js');
+
+    res.render('html', {
+        "htmlData": html,
+    });
+})
 // CSS stuff here
+app.get('/quiz/css', function(req, res) {
+    var css = require('./pages/css.js');
 
-app.get('/quiz/css', function(req, res) { // req = incoming request, res = outgoing response
-
-// Nick's code here, for comment go to css_old
-    // var questionData = {}
-    //     db.all('SELECT * FROM css', function(err, rows) {
-    //         rows.forEach((row) => {
-    //             questionData[row.id] = {
-    //                 "id": row.id,
-    //                 "question": row.question,
-    //                 "answer1": row.answer1,
-    //                 "answer2": row.answer2,
-    //                 "answer3": row.answer3,
-    //                 "answer4": row.answer4,
-    //                 "correct_answer": row.correct_answer
-    //             };
-    //         });
-    //     });
-    // db.close();
-    // module.exports = questionData
-
-
-    // Importing the backend code for the css page.
-    // Use this format to break main.js down into more manageable parts.
-    // The "var css" bit is what we will send to the pug template
-    var css = require('./pages/css_old.js');
-    // var css = questionData;
-    // Send (aka render) the page with the 'css' pug template.
-    res.render('css', { // res = outgoing response
-        // cssData is the variable we will use in the pug template
-        // css is the variable from line 164
+    res.render('css', {
         "cssData": css,
-        // cssData: css,
+    });
+})
+// Javascript stuff here
+app.get('/quiz/JS', function(req, res) {
+    var javascript = require('./pages/javascript.js');
 
+    res.render('javascript', {
+        "javascriptData": javascript,
     });
 })
 
--- pages/css.js
+++ pages/css.js
@@ -1,35 +1,73 @@
-app.get('/quiz/css', function(req, res) { // req = incoming request, res = outgoing response
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./hireme.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('CSS: Connected to the in-memory SQlite database.');
+});
 
-// Nick's code here, for comment go to css_old
-    var questionData = {}
-        db.all('SELECT * FROM new_css', function(err, rows) {
-            rows.forEach((row) => {
-                questionData[row.id] = {
-                    "id": row.id,
-                    "question": row.question,
-                    "answer1": row.answer1,
-                    "answer2": row.answer2,
-                    "answer3": row.answer3,
-                    "answer4": row.answer4,
-                    "correct_answer": row.correct_answer
-                };
-            });
-        });
-    db.close();
-    module.exports = questionData
+var questionData = {};
+db.all('SELECT * FROM css', function(err, rows) {
+  rows.forEach((row) => {
+
+    questionData[row.question] = {
+      "question": row.question,
+      "answer1": row.ans1,
+      "answer2": row.ans2,
+      "answer3": row.ans3,
+      "answer4": row.ans4,
+      "correct_answer": row.correctAns
+    }
+  })
+});
+
+db.close();
+module.exports = questionData;
 
 
-    // Importing the backend code for the css page.
-    // Use this format to break main.js down into more manageable parts.
-    // The "var css" bit is what we will send to the pug template
-    // var css = require('./pages/css_old.js');
-    var css = questionData;
-    // Send (aka render) the page with the 'css' pug template.
-    res.render('css', { // res = outgoing response
-        // cssData is the variable we will use in the pug template
-        // css is the variable from line 164
-        "cssData": css,
-        // cssData: css,
 
-    });
-})
+
+
+// app.get('/quiz/css', function(req, res) { // req = incoming request, res = outgoing response
+//
+//     // Nick's code here, for comment go to css_old
+//     var sqlite3 = require('sqlite3').verbose();
+//     var db = new sqlite3.Database('./hireme.db', (err) => {
+//       if (err) {
+//         return console.error(err.message);
+//       }
+//       console.log('CSS: Connected to the in-memory SQlite database.');
+//     });
+//
+//     var questionData = {}
+//     db.all('SELECT * FROM css', function(err, rows) {
+//         rows.forEach((row) => {
+//
+//             questionData[row.question] = {
+//
+//                 "question": row.question,
+//                 "answer1": row.ans1,
+//                 "answer2": row.ans2,
+//                 "answer3": row.ans3,
+//                 "answer4": row.ans4,
+//                 "correct_answer": row.correctAns
+//             }
+//         })
+//     });
+//     db.close();
+//     module.exports = questionData
+//
+//
+//     // Importing the backend code for the css page.
+//     // Use this format to break main.js down into more manageable parts.
+//     // The "var css" bit is what we will send to the pug template
+//     // var css = require('./pages/css_old.js');
+//     var css = questionData;
+//     // Send (aka render) the page with the 'css' pug template.
+//     res.render('css', { // res = outgoing response
+//         // cssData is the variable we will use in the pug template
+//         // css is the variable from line 164
+//         "cssData": css,
+//
+//     });
+// });
