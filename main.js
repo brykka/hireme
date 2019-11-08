f5f86d48b09ec428bdc72623154924006448e362
Bryenne Kay <bryenne.k@gmail.com>
2019-11-08 17:26:24 +0900

    server

--- main.js
+++ main.js
@@ -127,7 +127,7 @@ var listOfHtmlQuestions = {}
 var listOfCssQuestions = {}
 var listOfJsQuestions = {}
 
-var server = app.listen(8081, function() {
+var server = app.listen(process.env.PORT || 8081, function() {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
