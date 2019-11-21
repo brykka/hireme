d61eede1b3062a74ce9099e3724712db0b2f5d93
Bryenne Kay <bryenne.k@gmail.com>
2019-11-21 20:09:57 +0900

    working on question clink

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,7 @@ app.get('/', function(req, res) { // req = incoming request, res = outgoing resp
     var person1 = 'Ijen'
     var person2 = 'Bryenne'
     var person1Image = 'https://github.com/theyij.png?size=200.src'
-    var person2Image = 'https://github.com/Miimii1010.png?size=200'
-    var person3Image = 'https://avatars1.githubusercontent.com/u/47349194?s=460&v=4'
+    var person2Image = 'https://avatars1.githubusercontent.com/u/47349194?s=460&v=4'
     var linkedin = 'https://image.flaticon.com/icons/svg/1384/1384014.svg'
     var github = 'https://image.flaticon.com/icons/svg/25/25231.svg'
     var website = 'https://image.flaticon.com/icons/svg/1242/1242392.svg'
@@ -30,7 +29,7 @@ app.get('/', function(req, res) { // req = incoming request, res = outgoing resp
         person2: {
             name: person2,
             bio: "Continuously looking to imporve and expand my knowledge with interesting new projects.",
-            avatar: person3Image,
+            avatar: person2Image,
             linkedin: "https://www.linkedin.com/in/bryenne-kay/",
             github: "https://github.com/brykka",
             website: "https://bryennekay.com"
